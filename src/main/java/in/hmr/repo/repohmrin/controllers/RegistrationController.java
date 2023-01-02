4d5fa8b85ec372f3468a8394183f577f174275f4
Aditya Kaushik <addykaushik23@gmail.com>
2023-01-02 22:54:52 +0530

    RegistrationController not working with multipart file

--- src/main/java/in/hmr/repo/repohmrin/controllers/RegistrationController.java
+++ src/main/java/in/hmr/repo/repohmrin/controllers/RegistrationController.java
@@ -1,29 +1,38 @@
 package in.hmr.repo.repohmrin.controllers;
 
-import in.hmr.repo.repohmrin.userResponses.RegistrationDetails;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import in.hmr.repo.repohmrin.services.DatabaseEntryService;
+import in.hmr.repo.repohmrin.userResponses.RegistrationDetails;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("api/")
+@RequestMapping(value = "api/")
 public class RegistrationController {
+
     @Autowired
     private DatabaseEntryService databaseEntry;
 
     @PostMapping({"/register"})
-    public String userRegistration(@ModelAttribute RegistrationDetails registrationDetails) throws IOException {
+    @JsonDeserialize(as = RegistrationDetails.class)
+    public String userRegistration(@RequestBody RegistrationDetails registrationDetails) {
         System.out.println("received request");
         System.out.println(registrationDetails);
-        databaseEntry.addEntry(registrationDetails);
+//        System.out.println("file" + file.getOriginalFilename());
 
         return "We have your notes for review , " +
                 "if they are approved you will be able to see them on the website ," +
                 "Thank You for your contribution!";
     }
+//    @PostMapping({"/file"})
+//    public String userFile(@RequestParam("file") MultipartFile file) {
+//        System.out.println("received request");
+//        System.out.println("file" + file.getOriginalFilename());
+////        System.out.println(registrationDetails);
+//
+//        return "We have your notes for review , " +
+//                "if they are approved you will be able to see them on the website ," +
+//                "Thank You for your contribution!";
+//    }
 }
