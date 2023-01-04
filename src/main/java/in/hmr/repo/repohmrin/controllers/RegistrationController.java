a95316fe746c6b417517827f7904901cd002cbc9
aditya <addykaushik23@gmail.com>
2023-01-04 22:12:49 +0530

    received file in register api

--- src/main/java/in/hmr/repo/repohmrin/controllers/RegistrationController.java
+++ src/main/java/in/hmr/repo/repohmrin/controllers/RegistrationController.java
@@ -1,13 +1,9 @@
 package in.hmr.repo.repohmrin.controllers;
 
-import in.hmr.repo.repohmrin.userResponses.RegistrationDetails;
 import in.hmr.repo.repohmrin.services.DatabaseEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -16,14 +12,9 @@ public class RegistrationController {
     @Autowired
     private DatabaseEntryService databaseEntry;
 
-    @PostMapping({"/register"})
-    public String userRegistration(@ModelAttribute RegistrationDetails registrationDetails) throws IOException {
-        System.out.println("received request");
-        System.out.println(registrationDetails);
-        databaseEntry.addEntry(registrationDetails);
-
-        return "We have your notes for review , " +
-                "if they are approved you will be able to see them on the website ," +
-                "Thank You for your contribution!";
+    @PostMapping("/register")
+    public String uploadSingleFile (@RequestParam("file") MultipartFile file) {
+        System.out.println(file.getOriginalFilename());
+        return "file uploaded";
     }
 }
